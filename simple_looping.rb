0e26ef91dc8437ae701ed71374be76c18529ad70
Lillian Zhang <lilyzhangyun@gmail.com>
2018-06-01 17:32:58 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,23 +1,24 @@
-# REMEMBER: print your output to the terminal using 'puts'
-
-def loop_iterator(number_of_times)
-loop do 
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-    puts "Welcome to Flatiron School's Web Development Course!"
-end
-
-def times_iterator(number_of_times)
-7.times do
-    puts "Welcome to Flatiron School's Web Development Course!"
-end
 
 
-def while_iterator(number_of_times)
+def loop_iterator(7)
+  i=0
+  loop do 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    i+=1
+    break if i == 7
+  end
+end 
+
+
+
+def times_iterator(n)
+  7.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
+end 
+
+
+def while_iterator(n)
   counter = 0 
   while counter < 7 
   puts "Welcome to Flatiron School's Web Development Course!"
@@ -26,7 +27,8 @@ def while_iterator(number_of_times)
 end 
 
 
-def until_iterator(number_of_times)
+
+def until_iterator(n)
   counter = 0 
   until counter == 7 
     puts "Welcome to Flatiron School's Web Development Course!"
@@ -35,8 +37,12 @@ def until_iterator(number_of_times)
 end
 
 
-def for_iterator(number_of_times)
-for number in 1..7 do 
+
+def for_iterator(n)
+  n = 0
+  for n in 1..7 do 
    puts "Welcome to Flatiron School's Web Development Course!"
-end
+   n +=1
+  end
+end 
 
